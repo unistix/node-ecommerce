8d06160c6bed5ccc274466d681fa3b656bd7e47b
Cynny <c.dafinone@gmail.com>
2022-09-01 16:16:53 +0100

    featured controller and route added for test filtering

--- controllers/products.js
+++ controllers/products.js
@@ -1,13 +1,25 @@
+const Product = require('../models/products')
+
 const getAllProductsStatic = async (req, res) => {
-	throw new Error('testing async errors')
-	res.status(200).json({msg: 'products testing route'})
+	const products = await Product.find({})
+
+
+	//throw new Error('testing async errors')
+	res.status(200).json({products})
 }
 
 const getAllProducts = async (req, res) => {
 	res.status(200).json({msg: 'products route'})
 }
 
+
+const getAllProductsFeatured = async (req, res) => {
+	//hardcoded filter do-not use
+	const products = await Product.find({featured:true})
+	res.status(200).json({products , no:products.length})
+}
 module.exports = {
 	getAllProducts,
-	getAllProductsStatic
+	getAllProductsStatic,
+	getAllProductsFeatured
 }
--- routes/products.js
+++ routes/products.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const router = express.Router()
 
-const{getAllProducts, getAllProductsStatic} = require('../controllers/products')
+const{getAllProducts, getAllProductsStatic, getAllProductsFeatured} = require('../controllers/products')
 
 router.route('/').get(getAllProducts)
 router.route('/static').get(getAllProductsStatic)
+router.route('/featured').get(getAllProductsFeatured)
 
 module.exports = router
